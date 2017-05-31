6bb61519bd938892ca0182ced47b7b42c12195e5
davidtom <davidjtomczyk@gmail.com>
2017-05-31 13:42:37 -0400

    completed methods to pass test; may need more general solution

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,34 @@
-def alphabetize(arr)
-  # code here
+require "pry"
+
+def collect_letters(arr, index)
+  letter_array = []
+  arr.each { |phrase|
+    letter_array << phrase[index]
+  }
+  letter_array
 end
+
+def alphabetize(arr)
+  es_alph = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+
+  #Find first letter that all phrases differ on
+  index = 0
+  while true
+    if collect_letters(arr, index).uniq.length == 1
+      index += 1
+    elsif collect_letters(arr, index).uniq.length > 1
+      new_arr = arr.sort_by { |sentence|
+        es_alph.index(sentence[index])
+      }
+      return new_arr
+    else
+      puts "something is off"
+      break
+    end
+  end
+
+end
+
+arr = ["mi amas vin", "bonan matenon", "pacon", "ĉu vi parolas esperanton"]
+
+# puts alphabetize(arr)
